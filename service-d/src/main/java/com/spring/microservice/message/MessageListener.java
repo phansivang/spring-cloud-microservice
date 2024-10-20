0a63f8bda8915f90ecca4cff71502d8f427207a2
Phan <95892957+phansivang@users.noreply.github.com>
2024-10-20 22:55:19 +0700

    feat: microservice D

---
+++ service-d/src/main/java/com/spring/microservice/message/MessageListener.java
@@ -0,0 +1,29 @@
+package com.spring.microservice.message;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class MessageListener {
+
+    private final ThreadPoolTaskExecutor taskExecutor;
+
+    @Bean
+    public Consumer<String> message() {
+        return message -> taskExecutor.execute(
+                () -> {
+                    try {
+                        log.info("Kafka Message Received: {} your are on service D", message);
+                    } catch (Exception e) {
+                        log.error("Kafka Received message unexpected error", e);
+                    }
+                });
+    }
+}
